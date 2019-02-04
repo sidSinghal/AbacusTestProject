b8f4a149bfed0232818eb5a17a2dbbccd235ccfc
Siddhant Singhal <singhal.si@husky.neu.edu>
2019-02-04 04:20:30 -0500

    added DB file and config

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -13,3 +13,4 @@ INSERT INTO `person_api`.`person` (`id`, `name`, `age`, `locale`) VALUES ('p002'
 INSERT INTO `person_api`.`person` (`id`, `name`, `age`, `locale`) VALUES ('p003', 'Sai', '25', 'SF');
 INSERT INTO `person_api`.`person` (`id`, `name`, `age`, `locale`) VALUES ('p004', 'Rudy', '26', 'Houston');
 INSERT INTO `person_api`.`person` (`id`, `name`, `age`, `locale`) VALUES ('p005', 'Prabeen', '25', 'Kerela');
+//jdbc:mysql://localhost:3306/person_api?autoReconnect=true&useSSL=false
