ff270fac700a57674bae381d4c01f7f33c5faf67
Siddhant Singhal <singhal.si@husky.neu.edu>
2019-02-04 04:17:50 -0500

    added DB file and config

---
+++ src/main/resources/database.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE person_api;
+
+create table Person (
+	`id` varchar(30),
+    `name` varchar(30),
+    `age` int(5),
+    `locale` varchar(30),
+    primary key(`id`)
+);
+
+INSERT INTO `person_api`.`person` (`id`, `name`, `age`, `locale`) VALUES ('p001', 'Sid', '27', 'Boston');
+INSERT INTO `person_api`.`person` (`id`, `name`, `age`, `locale`) VALUES ('p002', 'Yash', '22', 'Boston');
+INSERT INTO `person_api`.`person` (`id`, `name`, `age`, `locale`) VALUES ('p003', 'Sai', '25', 'SF');
+INSERT INTO `person_api`.`person` (`id`, `name`, `age`, `locale`) VALUES ('p004', 'Rudy', '26', 'Houston');
+INSERT INTO `person_api`.`person` (`id`, `name`, `age`, `locale`) VALUES ('p005', 'Prabeen', '25', 'Kerela');
